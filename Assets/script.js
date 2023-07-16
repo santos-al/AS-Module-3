1e455560ae6c3f541518a96f4a67d4f1f35b84ca
Xandi <santosal2018@gmail.com>
2023-07-16 18:54:32 -0400

    add cahracter length check

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,27 @@
 // Assignment Code
+function generatePassword() {
+  let numer = false;
+  let specialChar = false;
+
+  // character length check for password
+  var charLength = window.prompt("Select the number of character (must be between 8 and 128)");
+  while (charLength < 8 || charLength > 128) {
+    charLength = window.prompt("Try again, please select a number between 8 and 128");
+  }
+  // Question for uppercase in password
+  let uppercase = window.confirm("Do you want uppercase letters in your password");
+  // Question for lowercase in password
+  let lowercase = window.confirm("Do you want lowercase letters in your password");
+  // check if at least uppercase or lowercase is slected or else loop back
+  while (uppercase || lowercase) {
+    uppercase = window.confirm("Do you want uppercase letters in your password");
+    lowercase = window.confirm("Do you want lowercase letters in your password");
+  }
+  return `lowercase is ${lowercase} and uppercase is ${uppercase}`;
+}
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
