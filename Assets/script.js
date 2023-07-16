9ac5f7bbaf76112901eb6e05dc904a76310d520b
Xandi <santosal2018@gmail.com>
2023-07-16 19:02:49 -0400

    add logic for special characters and numbers

--- Assets/script.js
+++ Assets/script.js
@@ -1,24 +1,36 @@
 // Assignment Code
 function generatePassword() {
-  let numer = false;
-  let specialChar = false;
 
   // character length check for password
   var charLength = window.prompt("Select the number of character (must be between 8 and 128)");
+
   while (charLength < 8 || charLength > 128) {
     charLength = window.prompt("Try again, please select a number between 8 and 128");
   }
+
   // Question for uppercase in password
   let uppercase = window.confirm("Do you want uppercase letters in your password?");
+
   // Question for lowercase in password
   let lowercase = window.confirm("Do you want lowercase letters in your password?");
-  // check if at least uppercase or lowercase is slected or else loop back
-  while (uppercase === false && lowercase === false) {
+
+  // check if at least uppercase or lowercase is selected or else loop back
+  while (uppercase && lowercase) {
     window.alert("At least either uppercase or lower case must be selected, choose again.")
     uppercase = window.confirm("Do you want uppercase letters in your password?");
     lowercase = window.confirm("Do you want lowercase letters in your password?");
   }
-  return `lowercase is ${lowercase} and uppercase is ${uppercase}`;
+
+  // Check for numbers in the password
+  let numeric = window.confirm("Do you want numbers in your password?");
+
+  // Check for special characters in the password
+  let specialChar = window.confirm("Do you want numbers in your password?");
+
+  return `numeric = ${numeric}, specialChar = ${specialChar}, 
+          uppercase = ${uppercase}, lowercase = ${lowercase}, 
+          and charLength = ${charLength}`
+
 }
 
 
