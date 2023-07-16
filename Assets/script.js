6d1338a1ce10bc32161b83f9575b4e5fd095de0b
Xandi <santosal2018@gmail.com>
2023-07-16 19:18:14 -0400

    add all possible characters for the password

--- Assets/script.js
+++ Assets/script.js
@@ -1,37 +1,41 @@
 // Assignment Code
 function generatePassword() {
 
-  // character length check for password
-  var charLength = window.prompt("Select the number of character (must be between 8 and 128)");
+  // Password options
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercase = "abcdefghijlmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const specialChar = "~`!@#$%^&*()_+-={}|[]:;'<,>.?/";
 
-  while (charLength < 8 || charLength > 128) {
-    charLength = window.prompt("Try again, please select a number between 8 and 128");
+  const allOptions = [uppercase, lowercase, numbers, specialChar];
+
+  // character length check for password
+  var charLengthCheck = window.prompt("Select the number of character (must be between 8 and 128)");
+
+  while (charLengthCheck < 8 || charLengthCheck > 128) {
+    charLengthCheck = window.prompt("Try again, please select a number between 8 and 128");
   }
 
   // Question for uppercase in password
-  let uppercase = window.confirm("Do you want uppercase letters in your password?");
+  let uppercaseCheck = window.confirm("Do you want uppercase letters in your password?");
 
   // Question for lowercase in password
-  let lowercase = window.confirm("Do you want lowercase letters in your password?");
+  let lowercaseCheck = window.confirm("Do you want lowercase letters in your password?");
 
   // Check for numbers in the password
-  let numeric = window.confirm("Do you want numbers in your password?");
+  let numericCheck = window.confirm("Do you want numbers in your password?");
 
   // Check for special characters in the password
-  let specialChar = window.confirm("Do you want special characters in your password?");
+  let specialCharCheck = window.confirm("Do you want special characters in your password?");
 
   // check if at least uppercase or lowercase is selected or else loop back
-  while (uppercase === false && lowercase === false && numeric === false && specialChar === false) {
+  while (uppercaseCheck === false && lowercaseCheck === false && numericCheck === false && specialCharCheck === false) {
     window.alert("At least one of the following must be selected: uppercase, lowercase, number, or special character.")
-    uppercase = window.confirm("Do you want uppercase letters in your password?");
-    lowercase = window.confirm("Do you want lowercase letters in your password?");
-    numeric = window.confirm("Do you want numbers in your password?");
-    specialChar = window.confirm("Do you want special characters in your password?");
+    uppercaseCheck = window.confirm("Do you want uppercase letters in your password?");
+    lowercaseCheck = window.confirm("Do you want lowercase letters in your password?");
+    numericCheck = window.confirm("Do you want numbers in your password?");
+    specialCharCheck = window.confirm("Do you want special characters in your password?");
   }
-
-  // return `numeric = ${numeric}, specialChar = ${specialChar}, 
-  //         uppercase = ${uppercase}, lowercase = ${lowercase}, 
-  //         and charLength = ${charLength}`
 
 }
 
